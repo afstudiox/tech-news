f9dab81f4494a05f4da10f7a3ccd8a27f9247b7c
Andre Felipe <afelipes@gmail.com>
2022-11-10 11:37:56 -0300

    feat(scraper): implements function fetch

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,24 @@
+import time
+
+import requests
+
+
 # Requisito 1
 def fetch(url):
     """Seu código deve vir aqui"""
+    for _ in range(3):
+        try:
+            response = requests.get(
+                url, headers={"User-Agent": "Fake user-agent"}, timeou=3
+            )
+            responseStatusCode = response.status_code
+            responseText = response.text
+            time.sleep(1)
+            if responseStatusCode != 200:
+                return None
+            return responseText
+        except requests.ReadTimeout:
+            return None
 
 
 # Requisito 2
