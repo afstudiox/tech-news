9dea0059593d8ece8fd3b2774429118c947f9d43
Andre Felipe <afelipes@gmail.com>
2022-11-11 01:25:44 -0300

    feat(scraper): implement function scrape_noticias

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -20,22 +20,36 @@ def fetch(url):
 
 
 # Requisito 2
-def scrape_novidades(html_content):
-    selector = Selector(text=html_content)
+def scrape_novidades(html_content):  # link da noticia
+    selector = Selector(html_content)
     return selector.css("h2.entry-title a::attr(href)").getall()
 
 
 # Requisito 3
-def scrape_next_page_link(html_content):
-    selector = Selector(text=html_content)
+def scrape_next_page_link(html_content):  # link da proxima pagina
+    selector = Selector(html_content)
     return selector.css("a.next::attr(href)").get()
 
 
 # Requisito 4
+# Impotância da canonical TAG https://rockcontent.com/br/blog/canonical-tag/
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    comments = selector.css("div.post-comments h5::text").get()
+    summary = selector.css("div.entry-content > p:nth-of-type(1) *::text")
+    tags = selector.css("a[rel=tag]::text").getall()
+    return {
+        "url": selector.css("link[rel=canonical]::attr(href)").get(),
+        "title": selector.css("h1.entry-title::text").get().strip(),
+        "timestamp": selector.css("li.meta-date::text").get(),
+        "writer": selector.css("span.author a::text").get(),
+        "comments_count": comments.split()[0] if comments else 0,
+        "summary": "".join(summary.getall()).strip(),
+        "tags": tags if tags else [],
+        "category": selector.css("a.category-style span.label::text").get(),
+    }
 
 
 # Requisito 5
 def get_tech_news(amount):
-    """Seu código deve vir aqui"""
+    selector = Selector(amount)
